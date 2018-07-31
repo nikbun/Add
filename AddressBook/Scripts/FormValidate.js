956bef9bdd05b6d3d261b725cc24d239bdc3dea7
Nikita Gorbunov <nik_dezel@mail.ru>
2018-07-31 12:42:14 +0300

    realised validate in secondary form

---
+++ AddressBook/Scripts/FormValidate.js
@@ -0,0 +1,81 @@
+﻿// Получаем все формы на странице
+var forms = document.getElementsByTagName('form');
+
+// К каждой форме прикрепляем слушатель
+for (var i = 0; i < forms.length; i++)
+{
+	forms[i].addEventListener('submit', validator());
+}
+
+// Список правил проверок
+var rules =
+	{
+		// Проверка на заполненность поля
+		required: function (el)
+		{
+			if (el.value != '')
+			{
+				return true;
+			}
+		return false;
+		},
+		// Проверка на емайл
+		email: function (el) {
+			var reg = /^\w{1,}@\w{1,}\w{2,}$/;
+			return reg.test(el.value);
+		}
+	}
+
+// Вывести ошибку в консоль
+function showErrors(arr)
+{
+	console.log(arr);
+}
+
+// Проверка полей
+function validator(e)
+{
+	e.preventDefault();
+	// Массив ошибок
+	var errors = [];
+	// Элементы формы
+	var inputs = this.elements;
+
+	// Обрабатываем каждый элемент формы
+	for (var i = 0; i < inputs.length; i++)
+	{
+		// Проверка на кнопку
+		if (inputs[i].tagName != 'BUTTON')
+		{
+			// Получаем из параметка data-rule строку правил проверки
+			var rulesList = inputs[i].dataset.rule;
+			// Перезаписываем строку в массив правил(правила разделены пробелом)
+			rulesList = rulesList.split(' ');
+
+			// Применяем все полученные правила для элемента
+			for (var j = 0; j < rulesList.length; j++)
+			{
+				// проверка на наличие соответствующего правила
+				if (rulesList[j] in rules)
+				{
+					// Если не проверка не прошло добавляем ошибку в массив ошибок
+					if (!rules[rulesList[j]](inputs[i]))
+					{
+						errors.push(
+							{
+							name: inputs[i].name,
+							error: rulesList[j]
+						});
+					}
+				}
+			}
+		}
+	}
+
+	// Выводит список ошибок в консоль
+	if (errors.length > 0)
+	{
+		e.preventDefault();
+		showErrors(errors);
+	}
+}
