8623e7bf97a0516a2d99e3e490f8e5860195c512
Никита Горбунов <Nik_dezel@mail.ru>
2018-08-08 18:18:48 +0300

    Added script database create

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿USE AddressBookDB;
+
+/* Добавление таблиц 
+DROP TABLE Addresses
+DROP TABLE TypeBuilding
+
+CREATE TABLE TypeBuilding (
+    BuildingId     INT		     NOT NULL,
+    TypeBuild NVARCHAR (50) NOT NULL,
+    CONSTRAINT primary_build_id PRIMARY KEY (BuildingId)
+);
+
+CREATE TABLE Addresses (
+    AddressId   INT               IDENTITY (1, 1) NOT NULL,
+    Country     NVARCHAR (50)     NOT NULL,
+    City        NVARCHAR (50)     NOT NULL,
+    Street      NVARCHAR (50)     NULL,
+    HouseNumber INT               NULL,
+    Date        DATETIME          DEFAULT (getdate()) NOT NULL,
+	TypeBuild   INT				  NOT NULL,
+	CONSTRAINT foreign_address    FOREIGN KEY (TypeBuild) REFERENCES TypeBuilding(BuildingId),
+    CONSTRAINT primary_address_id PRIMARY KEY (AddressID)
+);
+*/
