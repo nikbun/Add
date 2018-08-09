9df1c5f9342255d0c77cc5ac8c6caa2299beff94
Nikita Gorbunov <nik_dezel@mail.ru>
2018-08-09 23:23:06 +0300

    All done! Added form validate, off drop input information

--- AddressBook/Scripts/MyScripts/InputAddressFormListeners.js
+++ AddressBook/Scripts/MyScripts/InputAddressFormListeners.js
@@ -5,15 +5,21 @@
         var s = this.value;
         if (s != "") {
             s.substr(1, s.length);
-            s = s[0].toUpperCase() + s.substr(1, s.length);
+            s = s[0].toUpperCase() + s.substr(1, s.length).toLowerCase();
             this.value = s;
         }
     });
 
-    $("form#form1 input.not-e").keyup(function (eventObject) {
-        var s = this.value;
-        if (~s.indexOf('e')) {
-            this.value = s.replace("e", "")
-        }
+    // Перехватывает нажатия с клавиатуры позволяя вводить только цифры
+    $("form#form1 input.not-e").keydown(function (eventObject) {
+        if ((!(eventObject.which >= 48 && eventObject.which <= 57) ||
+            this.value.length >= 5) &&
+            eventObject.which != 8 &&
+            eventObject.which != 9 &&
+            eventObject.which != 13 &&
+            eventObject.which != 46)
+        {
+            eventObject.preventDefault();
+        }  
     });
 })
--- AddressBook/Scripts/MyScripts/loader.js
+++ AddressBook/Scripts/MyScripts/loader.js
@@ -1,3 +1,4 @@
 ﻿$(document).ready(function () {
     $("form#form1 select.form-control")[0].selectedIndex = 0;
+    $("form#form1 input#City")[0].autocomplete = "off"
 })
