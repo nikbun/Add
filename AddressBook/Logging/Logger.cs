059936b540c16e32cd2d6178e4e0c7907d63f043
Никита Горбунов <Nik_dezel@mail.ru>
2018-08-15 16:51:37 +0300

    Added loging

---
+++ AddressBook/Logging/Logger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace AddressBook.Logging
+{
+	public static class Logger
+	{
+		private const string logName = "AddressBookLOG";
+		private const string sourceName = "AddressBook";
+		private static EventLog log = new EventLog();
+
+		/// <summary>
+		/// Создает журнал если его нет
+		/// </summary>
+		public static void CreateLog()
+		{
+			if (!EventLog.SourceExists(sourceName))
+			{
+				var eventSourceData = new EventSourceCreationData(sourceName, logName);
+				EventLog.CreateEventSource(eventSourceData);
+			}
+		}
+
+		public static void WriteLog(string message, EventLogEntryType type)
+		{
+			log.Source = sourceName;
+			log.WriteEntry(message, type);
+		}
+	}
+}
